345a195b8e2fb6826dfe091900c376922be08d62
zenkoveric <zenkoveric@icloud.com>
2019-03-15 22:44:55 +1000

    Tried to add jump

--- control.py
+++ control.py
@@ -5,10 +5,16 @@ class Control:
     def __init__(self):
         self.flag_game = False
         self.flag_start = True
+        self.gravity = True
 
         self.clock = pygame.time.Clock()
         self.width = 1920#pygame.display.Info().current_w
         self.height = 1080 #pygame.display.Info().current_h
+        self.flag_jump = True
+
+        self.speed = 0
+
+        # self.myfont = pygame.font.SysFont(None, 96)
 
     def start(self, GUI):
         for event in pygame.event.get():
@@ -42,7 +48,23 @@ class Control:
                 if event.key == K_ESCAPE:
                     self.flag_game = False
 
+
+        '''posX = self.myfont.render('X' + str(doctor.x), False, (255, 255, 255))
+        posY = self.myfont.render('Y' + str(doctor.y), False, (255, 255, 255))
+
+        win.blit(posX, (100, 100))
+        win.blit(posY, (300, 100))'''
+
         keys = pygame.key.get_pressed()
+
+        # Gravity
+        if self.gravity:
+            if doctor.y < 950:
+                doctor.y += 20
+            if doctor.y > 950:
+                doctor.y -= 5
+
+        #Control
         if keys[K_d]:
             doctor.x += doctor.speed
         if keys[K_a]:
@@ -51,7 +73,20 @@ class Control:
             doctor.x += doctor.speed * 2
         if keys[K_a] and keys[K_LSHIFT]:
             doctor.x -= doctor.speed * 2'''
+        #Jump
 
+        if doctor.y == 950:
+            self.flag_jump = True
+        else:
+            self.flag_jump = False
+
+        '''if keys[K_SPACE] and self.flag_jump:
+            self.gravity = False
+            doctor.y -= 150
+        else:
+            self.gravity = True'''
+
+        self.speed += 10
         pygame.display.update()
         self.clock.tick(60)
 
--- player.py
+++ player.py
@@ -5,14 +5,15 @@ class Doctor:
 
     def __init__(self):
         self.x = 100
-        self.y = 800
+        self.y = 90
         self.speed = 10
 
         self.width = 80
-        self.height = 101
+        self.height = 100
 
     def draw(self, win):
         win.fill((0, 0, 0))
+        pygame.draw.rect(win, (0, 255, 0), (0, 0, 1920, 1080), 8)
         pygame.draw.rect(win, (0, 0, 255), (self.x, self.y, self.width, self.height))
 
     def animation(self):
